efbe7f624a043ca22c3fcd655ea141661710a868
Hoang An <tonnguyenhoangan@gmail.com>
2019-06-14 10:57:04 +0700

    sayHi for Dog

--- Dog.js
+++ Dog.js
@@ -1,7 +1,16 @@
-function Dog() {
+var chalk = require('chalk');
+
+function Dog(name) {
+    this.name = name;
     this.stomach = [];
 }
 
 Dog.prototype.eat = function(cat) {
     this.stomach.push(cat);
 }
+
+Dog.prototype.sayHi = function() {
+    console.log('Hi! I am a dog. My name is ' + chalk.red('this.name'));
+}
+
+module.exports = Dog;
